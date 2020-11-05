3b738059899fe9e0f9810123948725fcac23962b
d0p1er <nadykto.dmitry@gmail.com>
2020-11-05 04:21:30 +0300

    Proc 2.0.1

--- C/Proc/Data/commands.h
+++ C/Proc/Data/commands.h
@@ -7,27 +7,6 @@
 #define COMPAR(comp) if (DO_POP comp DO_POP)
 #define RIP rip = (int) cpu.tags[(int) value] - 1
 
-// #define BOLD "\x1B[1m"
-// #define KBCK  "\x1B[30m"
-// #define KRED  "\x1B[31m"
-// #define KGRN  "\x1B[32m"
-// #define KYEL  "\x1B[33m"
-// #define KBLU  "\x1B[34m"
-// #define KMAG  "\x1B[35m"
-// #define KCYN  "\x1B[36m"
-// #define KWHT  "\x1B[37m"
- 
-// #define BCK(x) KBCK x RST
-// #define RED(x) KRED x RST
-// #define GRN(x) KGRN x RST
-// #define YEL(x) KYEL x RST
-// #define BLU(x) KBLU x RST
-// #define MAG(x) KMAG x RST
-// #define WHT(x) KWHT x RST
- 
-// #define BOLD(x) "\x1B[1m" x RST
- 
-
 
 DEF_CMD(PUSH, 	N_CMD(0), 		MODE(1), 	ARG(1),	{rip++;
 													 switch((int) mode) {
